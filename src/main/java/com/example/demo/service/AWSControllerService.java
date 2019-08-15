7094639dbf7295017963f7e3e201ed00a8ef62ed
luckylyw19930104 <luckylyw19930104@Yingweis-MacBook-Pro.local>
2019-08-15 16:23:56 +0800

    IRIS

--- src/main/java/com/example/demo/service/AWSControllerService.java
+++ src/main/java/com/example/demo/service/AWSControllerService.java
@@ -1,127 +1,89 @@
 package com.example.demo.service;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.boot.origin.SystemEnvironmentOrigin;
 import org.springframework.stereotype.Service;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.StartInstancesRequest;
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.StopInstancesRequest;
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.DryRunResult;
-import com.amazonaws.services.ec2.model.DryRunSupportedRequest;
-import com.amazonaws.services.ec2.model.Filter;
-import com.amazonaws.services.ec2.model.RebootInstancesRequest;
-import com.amazonaws.services.ec2.model.RebootInstancesResult;
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.Address;
-import com.amazonaws.services.ec2.model.DescribeAddressesRequest;
-import com.amazonaws.services.ec2.model.DescribeAddressesResult;
-import com.amazonaws.services.ec2.AmazonEC2;
-import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
-import com.amazonaws.services.ec2.model.DescribeInstancesResult;
-import com.amazonaws.services.ec2.model.Instance;
-import com.amazonaws.services.ec2.model.Reservation;
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.StartInstancesRequest;
+import software.amazon.awssdk.services.ec2.model.StopInstancesRequest;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.StartInstancesRequest;
+import software.amazon.awssdk.services.ec2.model.StopInstancesRequest;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.DescribeInstancesRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeInstancesResponse;
+import software.amazon.awssdk.services.ec2.model.Filter;
+import software.amazon.awssdk.services.ec2.model.Instance;
+import software.amazon.awssdk.services.ec2.model.Reservation;
 
 @Service
 public class AWSControllerService {
+
+	public Ec2Client initEC2() {
+		AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
+			      "AKIAZDDLBNUNNAM2QXO4",
+			      "IknYIuzEF8LOL7ZIf1yUoqjLE2D4RFypClFDbpqF");
+		
+		Ec2Client ec2 = Ec2Client.builder().region(Region.AP_SOUTHEAST_1).credentialsProvider(StaticCredentialsProvider.create(awsCreds)).build();
+		return ec2;
+	}
 	
 	public void startEC2(String instance_id) {
-		final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withRegion("ap-southeast-1").build();
+		
+		Ec2Client ec2 = initEC2();
 
-        DryRunSupportedRequest<StartInstancesRequest> dry_request =
-            () -> {
-            StartInstancesRequest request = new StartInstancesRequest()
-                .withInstanceIds(instance_id);
+		StartInstancesRequest request = StartInstancesRequest.builder()
+		    .instanceIds(instance_id).build();
 
-            return request.getDryRunRequest();
-        };
-
-        DryRunResult dry_response = ec2.dryRun(dry_request);
-
-        if(!dry_response.isSuccessful()) {
-            System.out.printf(
-                "Failed dry run to start instance %s", instance_id);
-
-            throw dry_response.getDryRunResponse();
-        }
-
-        StartInstancesRequest request = new StartInstancesRequest()
-            .withInstanceIds(instance_id);
-
-        ec2.startInstances(request);
-
-        System.out.printf("Successfully started instance %s", instance_id);
+		ec2.startInstances(request);
 	}
 	
 	public void stopEC2(String instance_id) {
 		
-		BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAZDDLBNUNGHZCBRPI", "2U51EkO5vVWaImH8RAVpJwokbtcUz/1sVvCNr92U");
+		Ec2Client ec2 = initEC2();
 		
-		final AmazonEC2 ec2 = AmazonEC2ClientBuilder.standard().withRegion("ap-southeast-1").withCredentials(new AWSStaticCredentialsProvider(awsCreds)).build();
 
-        DryRunSupportedRequest<StopInstancesRequest> dry_request =
-            () -> {
-            StopInstancesRequest request = new StopInstancesRequest()
-                .withInstanceIds(instance_id);
+		StopInstancesRequest request = StopInstancesRequest.builder()
+		    .instanceIds(instance_id).build();
 
-            return request.getDryRunRequest();
-        };
+		ec2.stopInstances(request);
 
-        DryRunResult dry_response = ec2.dryRun(dry_request);
-
-        if(!dry_response.isSuccessful()) {
-            System.out.printf(
-                "Failed dry run to stop instance %s", instance_id);
-            throw dry_response.getDryRunResponse();
-        }
-
-        StopInstancesRequest request = new StopInstancesRequest()
-            .withInstanceIds(instance_id);
-
-        ec2.stopInstances(request);
-
-        System.out.printf("Successfully stop instance %s", instance_id);
 	}
 	
-	public void get() {
+	public ArrayList<String> getInstanceID(String ip) {
 		
-		final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
-		boolean done = false;
+		Ec2Client ec2 = initEC2();
+		
+        boolean done = false;
+        ArrayList instanceIDList = new ArrayList<String>();
+        Filter filter = Filter.builder().name("tag:Name").values("Test").build();
+        
+        // snippet-start:[ec2.java2.describe_instances.main]
+        String nextToken = null;
+        do {
+            DescribeInstancesRequest request = DescribeInstancesRequest.builder().filters(filter).build();
+            DescribeInstancesResponse response = ec2.describeInstances(request);
+            
+            for (Reservation reservation : response.reservations()) {
+                for (Instance instance : reservation.instances()) {
+                    instanceIDList.add(instance.instanceId());
+                }
+            }
+            nextToken = response.nextToken();
 
-		DescribeInstancesRequest request = new DescribeInstancesRequest();
-		while(!done) {
-		    DescribeInstancesResult response = ec2.describeInstances(request);
-
-		    for(Reservation reservation : response.getReservations()) {
-		        for(Instance instance : reservation.getInstances()) {
-		            System.out.printf(
-		                "Found instance with id %s, " +
-		                "AMI %s, " +
-		                "type %s, " +
-		                "state %s " +
-		                "and monitoring state %s",
-		                instance.getInstanceId(),
-		                instance.getImageId(),
-		                instance.getInstanceType(),
-		                instance.getState().getName(),
-		                instance.getMonitoring().getState());
-		        }
-		    }
-
-		    request.setNextToken(response.getNextToken());
-
-		    if(response.getNextToken() == null) {
-		        done = true;
-		    }
-		}
-	}
+        } while (nextToken != null);
+        
+        return instanceIDList;        
+    }
 }
--- src/main/java/com/example/demo/web/AWSControllerWeb.java
+++ src/main/java/com/example/demo/web/AWSControllerWeb.java
@@ -1,6 +1,10 @@
 package com.example.demo.web;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,23 +16,36 @@ import com.example.demo.service.AWSControllerService;
 @RequestMapping("EC2")
 public class AWSControllerWeb {
 	
+	@Value("${aws.access.key.id}")
+	private String access_key_id;
+	
+	@Value("${aws.secret.access.key}")
+	private String secret_access_key;
+	
 	@Autowired
 	AWSControllerService acs;
 	
 	@PostMapping("stop")
 	public void stopEC2() {
-		acs.stopEC2("i-0bbf4fb2cc1e2e760");
+		ArrayList<String> list = new ArrayList<>();
+		list = acs.getInstanceID("Test");
+		for(String l : list) {
+			System.out.println(l);
+			acs.stopEC2(l);
+		}
+		//acs.stopEC2("i-0bbf4fb2cc1e2e760");
 	}
 	
 	@PostMapping("start")
 	public void startEC2() {
-		acs.startEC2("i-0bbf4fb2cc1e2e760");
+		ArrayList<String> list = new ArrayList<>();
+		list = acs.getInstanceID("Test");
+		for(String l : list) {
+			System.out.println(l);
+			acs.startEC2(l);
+		}
 	}
 	
-	@PostMapping("get")
-	public void get() {
-		acs.get();
-	}
 	
 	@GetMapping("test")
 	public String test() {
